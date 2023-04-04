d7dbc4effdc781d5ac65df14dbd47bf284e4342a
RalucaSarbu <123745810+RalucaSarbu@users.noreply.github.com>
2023-04-04 23:19:23 +0300

    Add files via upload

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string pattern = textBox1.Text;
+            string FindPattern = textBox2.Text;
+            Match m = Regex.Match(FindPattern, pattern, RegexOptions.IgnoreCase);
+            if (m.Success)
+                richTextBox1.Text = "Found " + m.Value + " at position " + m.Index;
+                
+            
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
